9d0aad0528ebdf874fbad346acde660abfa27458
Gal Amouyal <galamouy@Gals-MacBook-Pro.local>
2024-03-13 12:32:54 +0200

    a

--- examples/apiRequest/index.js
+++ examples/apiRequest/index.js
@@ -4,12 +4,31 @@ const URL = "https://restcountries.com/v3.1/all";
 async function getCountries() {
   try {
     const result = await axios.get(URL);
-    const countries = result.data;
+    const { data } = result;
+    const countries = data;
     countries.map((c) => console.log(c?.name?.common));
   } catch (error) {
     console.log(error.message);
   }
 }
-getCountries();
+// getCountries();
+const RandomUsersUrl = "https://randomuser.me/api/?results=";
+async function getUsers(numberOfUsers) {
+  try {
+    if (typeof numberOfUsers !== "number")
+      throw new Error("number of users missing");
 
-const url2 = "https://randomuser.me/api/?results=10";
+    const result = await axios.get(RandomUsersUrl + numberOfUsers);
+    const { data } = result;
+    const { results } = data;
+    results.forEach((user) => {
+      console.log(
+        `Hi ${user?.name?.title}, full name: ${user?.name?.first} ${user?.name?.last}`
+      );
+    });
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+getUsers();
